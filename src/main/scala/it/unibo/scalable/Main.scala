10c9c8654c37f3be8949d640c8a2f16887dbd842
Alessandra Boccuto <aless97b@gmail.com>
2022-06-20 13:16:22 +0200

    separate target from features

--- src/main/scala/it/unibo/scalable/Main.scala
+++ src/main/scala/it/unibo/scalable/Main.scala
@@ -17,9 +17,15 @@ object Main {
     val datasetPath = args(0)
 
     val src = Source.fromFile(datasetPath)
-    val features = src.getLines.take(1).toList.head.split(",").map(_.trim)
 
+    // get features names (except for accident index) and target
+    val allFeats = src.getLines.take(1).toList.head.split(",").map(_.trim).tail
+    val features = allFeats.init
+    val target = allFeats.last
+
+    // get data as [[feature values], target]
     val data = ArrayBuffer[(Array[Float], Float)]()
+
     for (line <- src.getLines.drop(1).take(5000)){
       val row = line.split(',').map(_.trim)
       val toAdd = (row.tail.init.map(_.toFloat), row.tail.last.toFloat)
@@ -28,22 +34,24 @@ object Main {
 
     src.close()
 
-    val featFormats = List(Format.Ordered,Format.Ordered,Format.Unordered,Format.Ordered,
-      Format.Ordered,Format.Ordered,Format.Ordered,Format.Ordered,Format.Unordered,
-      Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Ordered,
-      Format.Unordered,Format.Ordered,Format.Unordered,Format.Ordered,Format.Ordered,
-      Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,
-      Format.Unordered,Format.Unordered,Format.Ordered,Format.Ordered,Format.Unordered,
-      Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,
-      Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,
-      Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,
-      Format.Unordered,Format.Unordered,Format.Unordered,Format.Ordered,Format.Ordered,
-      Format.Ordered,Format.Ordered,Format.Unordered)
+    // define data formats used by splitting handler
+    val featFormats = List(
+      Format.Ordered,Format.Ordered, Format.Unordered, Format.Ordered,Format.Ordered,Format.Ordered,Format.Ordered,
+      Format.Ordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,
+      Format.Ordered,Format.Unordered,Format.Ordered,Format.Unordered,Format.Ordered,Format.Ordered,Format.Unordered,
+      Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,
+      Format.Ordered,Format.Ordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,
+      Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,
+      Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,
+      Format.Unordered,Format.Unordered,Format.Unordered,Format.Ordered,Format.Ordered,Format.Ordered,Format.Ordered,
+      Format.Unordered)
+
+    // create decision tree
+    def placeholderMetric: Array[(Array[Float], Float)] => Float = (el:Array[(Array[Float], Float)]) => 0.1f
+    val placeholderDepth = 5
 
     val DT = new ParDecisionTree[Float]()
-    def placeholderMetric= (el:Array[(Float, Float)]) => 0.1f
-    val placeholderDepth = 5
-    DT.build(data.toArray, features.zip(featFormats), placeholderMetric, placeholderDepth)
+    DT.build(data.toArray, features.zip(featFormats), target, placeholderMetric, placeholderDepth)
 
     println(DT.root)
   }
