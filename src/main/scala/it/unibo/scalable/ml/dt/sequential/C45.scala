f22e60eb5665d124c969295dce54d5d12dae29ec
Alessandro Giacche <25297222+alejack9@users.noreply.github.com>
2022-09-04 18:37:30 +0200

    calculate information gain ratios

--- src/main/scala/it/unibo/scalable/ml/dt/sequential/C45.scala
+++ src/main/scala/it/unibo/scalable/ml/dt/sequential/C45.scala
@@ -15,42 +15,89 @@ object Types {
   type Attribute = (Format, Int)
 }
 
+object Utility {
+  def getMostFrequentTarget(ds: Dataset) = ds.map(row => (row.last, 1)).groupBy(_._1).map{case (a, b) => (a, b.length)}.maxBy(_._2)._1
+}
+
+
 class C45 {
 
   def run(ds: Dataset, attributeTypes: Seq[Format]): Tree[Float] = {
-    def _run(ds: Dataset, attributes: Seq[Attribute]) : Tree[Float]= {
-      if (attributes.isEmpty)
-        return Leaf(ds.map(row => (row.last, 1)).groupBy(_._1).map{case (a, b) => (a, b.length)}.maxBy(_._2)._1)
 
+    def _run(ds: Dataset, attributes: Seq[Attribute]) : Tree[Float]= {
+
+      if (ds.length == 1) return Leaf(ds.head.last)
+
+      if (attributes.isEmpty)
+        return Leaf(Utility.getMostFrequentTarget(ds))
+
+      // check node purity (all samples belongs to the same target)
+      if (ds.forall(_.last == ds.head.last)) return Leaf(ds.head.last)
 
       if (attributes.length == 1) {
         val attrIndex = attributes.head._2
-        val values = ds.map(sample => sample(attrIndex)).distinct
+        val attrValues = ds.map(sample => sample(attrIndex)).distinct
 
         if (attributes.head._1 == Format.Categorical) {
 
-          // 2, 3, 0, 4
+          def cond: Condition = new Condition(sample => attrValues.indexOf(sample(attrIndex)), s"feat $attrIndex $attrValues")
 
-          def cond: Seq[Float] => Int = sample => values.indexOf(sample(attrIndex))
-
-          CondNode(cond, values.map(v => _run(ds.filter(row => row(attrIndex) == v), attributes.patch(0, Nil, 1))))
+          return CondNode(cond, attrValues.map(v => _run(ds.filter(row => row(attrIndex) == v), attributes.patch(0, Nil, 1))))
         }
         else {
+          if (attrValues.length == 1) return Leaf(Utility.getMostFrequentTarget(ds))
+
           // [1,2,3,4] -> [1,2,3] [2,3,4] -> [(1,2), (2,3), (3,4)] => [1.5, 2.5, 3.5]
-          val midPoints = values.sorted.init.zip(values.sorted.tail).map{case (a, b) => (a + b) / 2.0 }
+          val midPoints = attrValues.sorted.init.zip(attrValues.sorted.tail).map{case (a, b) => (a + b) / 2.0 }
 
           val dsEntropy = Calc.entropy(ds)
 
-          // TODO
-          midPoints.map(midpoint => (midpoint, Calc.infoGainRatio(dsEntropy, branches, ds.length)))
+          val bestSplitPoint = midPoints.map(midpoint => {
+            val partitions = ds.partition(_ (attrIndex) < midpoint)
+            val partList = List(partitions._1, partitions._2)
+            (midpoint, Calc.infoGainRatio(dsEntropy, partList, ds.length), partList)
+          }).maxBy(_._2)
 
+          def cond: Condition = new Condition(sample => if (sample(attrIndex) < bestSplitPoint._1) 0 else 1, s"feat $attrIndex < ${bestSplitPoint._1}")
+
+          return CondNode(cond, bestSplitPoint._3.map(_run(_, attributes)))
         }
       }
 
+      // + 1 attributes are available
+      val dsEntropy = Calc.entropy(ds)
 
-      Leaf(-1)
+      val infoGainRatios: Seq[(Float, Condition)] = attributes.map{case (format, attrIndex) =>
+        val attrValues = ds.map(sample => sample(attrIndex)).distinct
+        if (attrValues.length == 1) (0.0f, new Condition(_ => -1))
+        else if (format == Format.Categorical) {
+          def cond: Condition = new Condition(sample => attrValues.indexOf(sample(attrIndex)), s"feat $attrIndex $attrValues")
+
+          (Calc.infoGainRatio(dsEntropy, ds.groupBy(sample => sample(attrIndex)).values.toList, ds.length), cond)
+        } else {
+          val midPoints = attrValues.sorted.init.zip(attrValues.sorted.tail).map { case (a, b) => (a + b) / 2.0 }
+
+          val bestSplitPoint = midPoints.map(midpoint => {
+            val partitions = ds.partition(_ (attrIndex) < midpoint)
+            val partList = List(partitions._1, partitions._2)
+            (midpoint, Calc.infoGainRatio(dsEntropy, partList, ds.length), partList)
+          }).maxBy(_._2)
+
+          def cond: Condition = new Condition(sample => if (sample(attrIndex) < bestSplitPoint._1) 0 else 1, s"feat $attrIndex < ${bestSplitPoint._1}")
+
+          (bestSplitPoint._2, cond)
+        }
+      }
+
+      println(infoGainRatios)
+
+      // TODO: take best infoGainRatio and make a node with that
+      ???
+
+
+      throw new NotImplementedError()
     }
-    print(ds.head.init.indices.toList)
+
     _run(ds, attributeTypes.zipWithIndex)
   }
 }
--- src/test/scala/it/unibo/scalable/ml/dt/sequential/C45Test.scala
+++ src/test/scala/it/unibo/scalable/ml/dt/sequential/C45Test.scala
@@ -1,25 +1,60 @@
 package it.unibo.scalable.ml.dt.sequential
 
+import it.unibo.scalable.ml.dt.{CondNode, Condition, Leaf, Tree}
 import org.scalatest.funsuite.AnyFunSuite
 
 class C45Test extends AnyFunSuite {
   val D: Seq[Seq[Float]] = List(
-    Seq(0, 3, 3, 5, 0),
-    Seq(1, 1, 1, 7, 0),
-    Seq(2, 1, 2, 11, 2),
-    Seq(3, 1, 9, 11, 2),
-    Seq(4, 7, 1, 3, 2)
+    Seq(3, 3, 5, 0),
+    Seq(1, 1, 7, 0),
+    Seq(1, 2, 11, 2),
+    Seq(1, 9, 11, 2),
+    Seq(7, 1, 3, 2)
   ).map(_.map(_.toFloat))
 
   val dtc = new C45
 
-  test("run") {
-    println("__________Dataset__________")
-    println("| row  a_0  a_1  a_2    c |")
-    println("|-------------------------|")
-    D.sortBy(_.head).foreach{row => println("| % 3.0f  % 3.0f  % 3.0f  % 3.0f  % 3.0f |".format(row(0), row(1), row(2), row(3), row(4)))}
-    println("---------------------------")
-    dtc.run(D)
-    assert(1==1)
+  test("a_0, continuous") {
+//    println("________Dataset_______")
+//    println("| a_0  a_1  a_2    c |")
+//    println("|--------------------|")
+//    D.sortBy(_.head).foreach{row => println("| % 3.0f  % 3.0f  % 3.0f  % 3.0f |".format(row(0), row(1), row(2), row(3)))}
+//    println("----------------------")
+    val t = dtc.run(D.map(sample => List(sample.head, sample.last)), List(Format.Continuous))
+//    t.show
+    assert(t == CondNode(new Condition(_ => 0, "feat 0 < 5.0"), List(CondNode(new Condition(_ => 0, "feat 0 < 2.0"), List(Leaf(2.0), Leaf(0.0))), Leaf(2.0))).asInstanceOf[Tree[Float]])
+  }
+
+  test("a_0, categorical") {
+    //    println("________Dataset_______")
+    //    println("| a_0  a_1  a_2    c |")
+    //    println("|--------------------|")
+    //    D.sortBy(_.head).foreach{row => println("| % 3.0f  % 3.0f  % 3.0f  % 3.0f |".format(row(0), row(1), row(2), row(3)))}
+    //    println("----------------------")
+    val t = dtc.run(D.map(sample => List(sample.head, sample.last)), List(Format.Categorical))
+    //    t.show
+    assert(t == CondNode(new Condition(_ => 0, "feat 0 List(3.0, 1.0, 7.0)"), List(Leaf(0.0), Leaf(2.0), Leaf(2.0))).asInstanceOf[Tree[Float]])
+  }
+
+  test("a0 a1 continuous") {
+    println("________Dataset_______")
+    println("| a_0  a_1  a_2    c |")
+    println("|--------------------|")
+    D.sortBy(_.head).foreach{row => println("| % 3.0f  % 3.0f  % 3.0f  % 3.0f |".format(row(0), row(1), row(2), row(3)))}
+    println("----------------------")
+    val t = dtc.run(D.map(sample => List(sample.head, sample(1), sample.last)), List(Format.Continuous, Format.Continuous))
+    //    t.show
+//    assert(t == CondNode(new Condition(_ => 0, "feat 0 List(3.0, 1.0, 7.0)"), List(Leaf(0.0), Leaf(2.0), Leaf(2.0))).asInstanceOf[Tree[Float]])
+  }
+
+  test("a0 a1 mixed") {
+    println("________Dataset_______")
+    println("| a_0  a_1  a_2    c |")
+    println("|--------------------|")
+    D.sortBy(_.head).foreach{row => println("| % 3.0f  % 3.0f  % 3.0f  % 3.0f |".format(row(0), row(1), row(2), row(3)))}
+    println("----------------------")
+    val t = dtc.run(D.map(sample => List(sample.head, sample(1), sample.last)), List(Format.Continuous, Format.Categorical))
+    //    t.show
+//    assert(t == CondNode(new Condition(_ => 0, "feat 0 List(3.0, 1.0, 7.0)"), List(Leaf(0.0), Leaf(2.0), Leaf(2.0))).asInstanceOf[Tree[Float]])
   }
 }
