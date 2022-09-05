da51078b8713088f7757a54fd02d29d78c2979b5
Alessandro Giacche <25297222+alejack9@users.noreply.github.com>
2022-09-05 18:10:14 +0200

    Generic Condition

--- src/main/scala/it/unibo/scalable/ml/dt/Condition.scala
+++ src/main/scala/it/unibo/scalable/ml/dt/Condition.scala
@@ -1,7 +1,22 @@
 package it.unibo.scalable.ml.dt
 
-sealed class Condition(private val fun: Seq[Float] => Int, desc: String = "") extends (Seq[Float] => Int) {
-  def !=(v1: Condition): Boolean = v1.toString != this.toString
-  override def apply(v1: Seq[Float]): Int = fun(v1)
+sealed trait Condition[T] extends (Seq[T] => Int) {
+  protected def fun: Seq[T] => Int
+  protected val desc: String
+  def !=(v1: Condition[T]): Boolean = v1.toString != this.toString
+  override def apply(v1: Seq[T]): Int = fun(v1)
   override def toString: String = desc
+}
+
+case class ContinuousCondition[T : Ordering](private val index: Int, private val threshold: T)
+  extends Condition[T] {
+  private val ord: Ordering[T] = implicitly[Ordering[T]]
+  override def fun = (sample: Seq[T]) => if(ord.lt(sample(index), threshold)) 0 else 1
+  override val desc = s"feat $index < $threshold"
+}
+
+case class CategoricalCondition[T](private val index: Int, private val attrValues: Seq[T])
+  extends Condition[T] {
+  override def fun = (sample: Seq[T]) => attrValues.indexOf(sample(index))
+  override val desc = s"feat $index $attrValues"
 }
