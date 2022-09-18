e7a9ab44be4c039b47c4882baeb79163caa9af46
Alessandro Giacche <25297222+alejack9@users.noreply.github.com>
2022-09-18 15:21:47 +0200

    seq in main

--- src/main/scala/it/unibo/scalable/Main.scala
+++ src/main/scala/it/unibo/scalable/Main.scala
@@ -1,11 +1,11 @@
 package it.unibo.scalable
 
-import it.unibo.scalable.ml.dt.Format
-import it.unibo.scalable.ml.dt.PLANET.Controller
-import it.unibo.scalable.ml.dt.spark.C45
-import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkConf, SparkContext}
-//import org.apache.spark.ml.classification.DecisionTreeClassifier
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+import scala.io.Source
+import it.unibo.scalable.ml.dt.sequential.C45
+import it.unibo.scalable.ml.dt.sequential.Format
+
+//import it.unibo.scalable.ml.dt.spark.{C45, ContextFactory}
 
 object Main {
   def main(args : Array[String]): Unit = {
@@ -13,25 +13,42 @@ object Main {
       println("No args")
       return
     }
-    val n_threads = "*"
 
     val datasetPath = args(0)
-    val conf = new SparkConf().setAppName("Accident-Severity-Prediction").setMaster("local[" + n_threads + "]").set("spark.driver.maxResultSize", "0")
-    val sc = new SparkContext(conf)
 
-    val rdd = sc.textFile(datasetPath)
-    val dataset = rdd
-      .mapPartitionsWithIndex{(idx, iter) => if(idx == 0) iter.drop(1) else iter}
-      .map(row => row.split(",").toSeq)
-      .map(_.drop(1))
-      .map(_.map(_.toFloat))
-//      .map(row => (row.reverse.head, row.reverse.tail))
-//      .map{case (y, xs) => (y.toInt, xs.map(_.toFloat))}
-    print(dataset.collect.mkString("Array(\r\n", "\r\n", ")"))
-    val c45 = new C45
-    c45.run(dataset)
-//    def giniIndex(ys: Seq[Float]): Float = 1 - (ys.distinct.map(c => math.pow(ys.count(_ == c) / ys.length.toFloat, 2).toFloat) aggregate 0.0.toFloat)((a, e) => a + e, _+_)
-//    val controller = new Controller(_ < 10, giniIndex)
-//    controller.run(dataset, List(Format.Ordered,Format.Ordered,Format.Unordered,Format.Ordered,Format.Ordered,Format.Ordered,Format.Ordered,Format.Ordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Ordered,Format.Unordered,Format.Ordered,Format.Unordered,Format.Ordered,Format.Ordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Ordered,Format.Ordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Unordered,Format.Ordered,Format.Ordered,Format.Ordered,Format.Ordered,Format.Unordered))
+    val src = Source.fromFile(datasetPath)
+
+    val x: Iterator[Seq[Float]] = src.getLines.slice(1, 1000).map(r => r.split(',').map(_.trim).tail.map(_.toFloat))
+
+    val featFormats = List(
+      Format.Continuous,Format.Continuous, Format.Categorical, Format.Continuous,Format.Continuous,Format.Continuous,Format.Continuous,
+      Format.Continuous,Format.Categorical,Format.Categorical,Format.Categorical,Format.Continuous,Format.Categorical,
+      Format.Continuous,Format.Categorical,Format.Continuous,Format.Categorical,Format.Continuous,Format.Continuous,Format.Categorical,
+      Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,
+      Format.Continuous,Format.Continuous,Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,
+      Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,
+      Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,
+      Format.Categorical,Format.Categorical,Format.Categorical,Format.Continuous,Format.Continuous,Format.Continuous,Format.Continuous,
+      Format.Categorical)
+
+    val dt = new C45().run(x.toSeq, featFormats)
+    dt.show
+
+
+//
+//    val sc = ContextFactory.getContext()
+//
+//    val rdd = sc.textFile(datasetPath)
+//    val dataset = rdd
+////      .mapPartitionsWithIndex{(idx, iter) => if(idx == 0) iter.drop(1) else iter}
+//      .map(row => row.split(",").toSeq)
+//      .map(_.drop(1))
+//      .map(_.map(_.toFloat))
+////      .map(row => (row.reverse.head, row.reverse.tail))
+////      .map{case (y, xs) => (y.toInt, xs.map(_.toFloat))}
+//    print(dataset.collect.mkString("[\r\n", "\r\n", "]"))
+//    val c45 = new C45
+//    c45.run(dataset)
+////    def giniIndex(ys: Seq[Float]): Float = 1 - (ys.distinct.map(c => math.pow(ys.count(_ == c) / ys.length.toFloat, 2).toFloat) aggregate 0.0.toFloat)((a, e) => a + e, _+_)
   }
 }
