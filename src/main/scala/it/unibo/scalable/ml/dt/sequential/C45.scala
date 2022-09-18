915f86cc6d5f0bc1fb9aff7192cbe2aa6cc930c8
Alessandro Giacche <25297222+alejack9@users.noreply.github.com>
2022-09-18 15:17:00 +0200

    refactoring C4.5 seq

--- src/main/scala/it/unibo/scalable/ml/dt/sequential/C45.scala
+++ src/main/scala/it/unibo/scalable/ml/dt/sequential/C45.scala
@@ -11,64 +11,61 @@ object Format extends Enumeration {
 }
 
 object Types {
-  type Dataset = Seq[Seq[Float]]
+  type Dataset[T <: Seq[Float]] = Seq[T]
   type Attribute = (Format, Int)
 }
 
-object Utility {
-  def getMostFrequentTarget(ds: Dataset): Float = ds.map(row => (row.last, 1)).groupBy(_._1).map{case (a, b) => (a, b.length)}.maxBy(_._2)._1
-  // def getBranchesFromCondition(ds: Dataset, condition: Condition): Seq[Dataset] = ds.groupBy(sample => condition(sample)).values.toList
-}
-
 class C45 {
+  private def bestContinuousSplitPoint[T <: Seq[Float]](ds: Dataset[T], dsEntropy: Float, attrValues: Seq[Float], attrIndex: Int)
+    : (ContinuousCondition[Float], Float, Seq[Dataset[T]]) = {
+      // [1,2,3,4] -> [1,2,3] [2,3,4] -> [(1,2), (2,3), (3,4)] => [1.5, 2.5, 3.5]
+      val midPoints = attrValues.sorted.init.zip(attrValues.sorted.tail).map{case (a, b) => (a + b) / 2.0 }
 
-  def run(ds: Dataset, attributeTypes: Seq[Format]): Tree[Float] = {
+      val bestSplitPoint = midPoints.map(midpoint => {
+        val partitions = ds.partition(row => row(attrIndex) < midpoint)
+        val partList = List(partitions._1, partitions._2)
+        (midpoint, Calc.infoGainRatio(dsEntropy, partList, ds.length), partList)
+      }).maxBy(_._2)
 
-    def _run(ds: Dataset, attributes: Seq[Attribute]) : Tree[Float]= {
+      (ContinuousCondition(attrIndex, bestSplitPoint._1.toFloat), bestSplitPoint._2, bestSplitPoint._3)
+  }
+
+  // the last value of each sample represents the class target
+  def run[T <: Seq[Float]](ds: Dataset[T], attributeTypes: Seq[Format]): Tree[Float] = {
+
+    def _run(ds: Dataset[T], attributes: Seq[Attribute]) : Tree[Float]= {
 
       if (ds.length == 1) return Leaf(ds.head.last)
 
-      if (attributes.isEmpty)
-        return Leaf(Utility.getMostFrequentTarget(ds))
+      if (attributes.isEmpty) return LeafFactory.get(ds)
 
       // check node purity (all samples belongs to the same target)
       if (ds.forall(_.last == ds.head.last)) return Leaf(ds.head.last)
 
+      // only an attribute left
       if (attributes.length == 1) {
         val attrIndex = attributes.head._2
         val attrValues = ds.map(sample => sample(attrIndex)).distinct
 
-        if (attributes.head._1 == Format.Categorical) {
-
-          def cond = CategoricalCondition(attrIndex, attrValues)
-
-          return CondNode(cond, attrValues.map(v => _run(ds.filter(row => row(attrIndex) == v), attributes.patch(0, Nil, 1))))
-        }
+        if (attributes.head._1 == Format.Categorical)
+          return CondNode(
+            CategoricalCondition(attrIndex, attrValues),
+            attrValues.map(v => _run(ds.filter(row => row(attrIndex) == v), attributes.patch(0, Nil, 1)))
+          )
         else {
-          if (attrValues.length == 1) return Leaf(Utility.getMostFrequentTarget(ds))
+          if (attrValues.length == 1) return LeafFactory.get(ds)
 
-          // [1,2,3,4] -> [1,2,3] [2,3,4] -> [(1,2), (2,3), (3,4)] => [1.5, 2.5, 3.5]
-          val midPoints = attrValues.sorted.init.zip(attrValues.sorted.tail).map{case (a, b) => (a + b) / 2.0 }
-
-          val dsEntropy = Calc.entropy(ds)
-
-          val bestSplitPoint = midPoints.map(midpoint => {
-            val partitions = ds.partition(_ (attrIndex) < midpoint)
-            val partList = List(partitions._1, partitions._2)
-            (midpoint, Calc.infoGainRatio(dsEntropy, partList, ds.length), partList)
-          }).maxBy(_._2)
-
-          def cond = ContinuousCondition(attrIndex, bestSplitPoint._1.toFloat)
-
-          return CondNode(cond, bestSplitPoint._3.map(_run(_, attributes)))
+          val (cond, _, subDss) = bestContinuousSplitPoint(ds, Calc.entropy(ds), attrValues, attrIndex)
+          return CondNode(cond, subDss.map(_run(_, attributes)))
         }
       }
 
       // + 1 attributes are available
       val dsEntropy = Calc.entropy(ds)
 
-      val infoGainRatios: Seq[(Float, Condition[_ <: Float], Seq[Dataset], Int)] = attributes.zipWithIndex.map{case ((format, attrIndex), index) =>
+      val infoGainRatios: Seq[(Float, Condition[_ <: Float], Seq[Dataset[T]], Int)] = attributes.zipWithIndex.map{case ((format, attrIndex), index) =>
         val attrValues = ds.map(sample => sample(attrIndex)).distinct
+
         if (attrValues.length == 1) (0.0f, CategoricalCondition(index, Nil), Nil, -1)
         else if (format == Format.Categorical) {
           def cond: Condition[Float] = CategoricalCondition(attrIndex, attrValues)
@@ -76,21 +73,13 @@ class C45 {
           val branches = ds.groupBy(sample => sample(attrIndex)).values.toList
           (Calc.infoGainRatio(dsEntropy, branches, ds.length), cond, branches, index)
         } else { // continuous case
-          val midPoints = attrValues.sorted.init.zip(attrValues.sorted.tail).map { case (a, b) => (a + b) / 2.0 }
 
-          val bestSplitPoint = midPoints.map(midpoint => {
-            val partitions = ds.partition(_ (attrIndex) < midpoint)
-            val partList = List(partitions._1, partitions._2)
-            (midpoint, Calc.infoGainRatio(dsEntropy, partList, ds.length), partList)
-          }).maxBy(_._2)
-
-          def cond: Condition[Float] = ContinuousCondition(attrIndex, bestSplitPoint._1.toFloat)
-
-          (bestSplitPoint._2, cond, bestSplitPoint._3, index)
+          val (cond, infoGainRatio, subDss) = bestContinuousSplitPoint(ds, dsEntropy, attrValues, attrIndex)
+          (infoGainRatio, cond, subDss, index)
         }
       }
 
-      if (infoGainRatios.forall(_._1 == 0.0f)) return Leaf(Utility.getMostFrequentTarget(ds))
+      if (infoGainRatios.forall(_._1 == 0.0f)) return LeafFactory.get(ds)
 
       val maxGainRatio = infoGainRatios.maxBy(_._1)
 
