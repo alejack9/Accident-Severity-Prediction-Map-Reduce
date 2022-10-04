6db7e3d9ac267011e3fbda03f2dea62baf512fd9
Michele Celozzi <michele.celozzi97@gmail.com>
2022-10-04 13:12:03 +0200

    added train and test sets

--- src/main/scala/it/unibo/scalable/Main.scala
+++ src/main/scala/it/unibo/scalable/Main.scala
@@ -10,25 +10,38 @@ import it.unibo.scalable.ml.dt.sequential.Format
 object Main {
   def main(args : Array[String]): Unit = {
     if (args.length == 0) {
-      println("No args")
-      return
+      println("Training ds not provided")
+      sys.exit(-1)
+    }
+    if (args.length == 1) {
+      println("Test ds not provided")
+      sys.exit(-1)
     }
 
     // test the alg with 1% -> 42876 samples , 5% -> 214380 samples and 10% -> 428759 samples of the original dataset,
     // val testSizeRates = Array(1, 5, 10)
-    val datasetPath = args(0)
+    val trainDSPath = args(0)
+    val testDSPath = args(1)
 
-    val src = Source.fromFile(datasetPath)
-    val data = new ArrayBuffer[Seq[Float]]();
+    val trainSrc = Source.fromFile(trainDSPath)
+    val testSrc = Source.fromFile(trainDSPath)
+    val trainData = new ArrayBuffer[Seq[Float]]()
+    val testData = new ArrayBuffer[Seq[Float]]()
 
-    for ((line, idx) <- src.getLines.drop(1).zipWithIndex) {
-//      if (idx % 10000 == 0)
-//        println(idx + " rows read")
-      data += line.split(',').tail.map(_.trim.toFloat).toSeq
+    for ((line, idx) <- trainSrc.getLines.drop(1).zipWithIndex) {
+      //      if (idx % 10000 == 0)
+      //        println(idx + " rows read")
+      trainData += line.split(',').tail.map(_.trim.toFloat).toSeq
+    }
+
+    for ((line, idx) <- testSrc.getLines.drop(1).zipWithIndex) {
+      //      if (idx % 10000 == 0)
+      //        println(idx + " rows read")
+      testData += line.split(',').tail.map(_.trim.toFloat).toSeq
     }
 
     // read mode changed because it got overhead error
-    //val x: Iterator[Seq[Float]] = src.getLines.drop(1).map(r => r.split(',').map(_.trim).tail.map(_.toFloat))
+    //val x: Iterator[Seq[Float]] = trainSrc.getLines.drop(1).map(r => r.split(',').map(_.trim).tail.map(_.toFloat))
 
     val featFormats = List(
       Format.Continuous,Format.Continuous, Format.Categorical, Format.Continuous,Format.Continuous,Format.Continuous,Format.Continuous,
@@ -42,26 +55,25 @@ object Main {
       Format.Categorical)
 
 
-    val y = data.toArray.toSeq
+    val y = trainData.toArray.toSeq
     val c45 = new C45
-    val t1 = System.nanoTime
-    c45.run(y, featFormats) //.show
-    val t2 = System.nanoTime() - t1
-    println(t2 / 1e9d)
-
-//    val t1 = System.nanoTime
-//    val dt_time = c45.run(y, featFormats)
-//    val t2 = System.nanoTime() - t1
-
-//    val dt = new C45().run(x.toSeq, featFormats)
-//    dt_time.show
-
-//    println(t2 / 1e9d)
+    var t1 = System.nanoTime
+    val tree = c45.train(y, featFormats) //.show
+    val trainTime = System.nanoTime - t1
+    t1 = System.nanoTime
+    val predictedYs = tree.predict(testData)
+    val testTime = System.nanoTime - t1
+    val score = tree.score(testData, predictedYs)
+    println("{" +
+      "  trainTime: "   + trainTime / 1e9d +
+      ", testTime: " + testTime / 1e9d +
+      ", score: " + score +
+      "}")
 
 //
 //    val sc = ContextFactory.getContext()
 //
-//    val rdd = sc.textFile(datasetPath)
+//    val rdd = sc.textFile(trainDSPath)
 //    val dataset = rdd
 ////      .mapPartitionsWithIndex{(idx, iter) => if(idx == 0) iter.drop(1) else iter}
 //      .map(row => row.split(",").toSeq)
