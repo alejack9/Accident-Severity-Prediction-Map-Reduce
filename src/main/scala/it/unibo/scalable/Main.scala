85d7f8771910ea25ee8a8cdc4d90a6c299dd541d
Alessandro Giacchè <25297222+alejack9@users.noreply.github.com>
2022-11-05 00:57:04 +0100

    add spark to main

--- src/main/scala/it/unibo/scalable/Main.scala
+++ src/main/scala/it/unibo/scalable/Main.scala
@@ -4,6 +4,7 @@ import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 import it.unibo.scalable.ml.dt._
 import it.unibo.scalable.ml.dt.Utils._
+import it.unibo.scalable.ml.dt.spark._
 
 object Main {
   def main(args : Array[String]): Unit = {
@@ -23,69 +24,87 @@ object Main {
       sys.exit(-1)
     }
 
-    val modes = Array("seq", "par")
+    val modes = Array("seq", "par", "spark")
 
     if (!modes.contains(args(2))) {
       println("Available computation modes: " + modes.mkString(", "))
       sys.exit(-1)
     }
 
-    // test the alg with 1% -> 42876 samples , 5% -> 214380 samples and 10% -> 428759 samples of the original dataset,
-    // val testSizeRates = Array(1, 5, 10)
     val trainDSPath = args(0)
     val testDSPath = args(1)
 
-    val trainSrc = Source.fromFile(trainDSPath)
-    val testSrc = Source.fromFile(testDSPath)
+    if(args(2) != "spark") {
+      // test the alg with 1% -> 42876 samples , 5% -> 214380 samples and 10% -> 428759 samples of the original dataset,
+      // val testSizeRates = Array(1, 5, 10)
 
-    val trainData = new ArrayBuffer[Seq[Float]]()
-    val testData = new ArrayBuffer[Seq[Float]]()
+      val trainSrc = Source.fromFile(trainDSPath)
+      val testSrc = Source.fromFile(testDSPath)
 
-    for (line <- trainSrc.getLines.drop(1))
-      trainData += line.split(',').tail.map(_.trim.toFloat).toSeq
+      val trainData = new ArrayBuffer[Seq[Float]]()
+      val testData = new ArrayBuffer[Seq[Float]]()
 
-    for (line <- testSrc.getLines.drop(1))
-      testData += line.split(',').tail.map(_.trim.toFloat).toSeq
+      for (line <- trainSrc.getLines.drop(1))
+        trainData += line.split(',').tail.map(_.trim.toFloat).toSeq
 
-    // read mode changed because it got overhead error
-    //val x: Iterator[Seq[Float]] = trainSrc.getLines.drop(1).map(r => r.split(',').map(_.trim).tail.map(_.toFloat))
-    val featFormats = List(
-      Format.Continuous,Format.Continuous, Format.Categorical, Format.Continuous,Format.Continuous,Format.Continuous,Format.Continuous,
-      Format.Continuous,Format.Categorical,Format.Categorical,Format.Categorical,Format.Continuous,Format.Categorical,
-      Format.Continuous,Format.Categorical,Format.Continuous,Format.Categorical,Format.Continuous,Format.Continuous,Format.Categorical,
-      Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,
-      Format.Continuous,Format.Continuous,Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,
-      Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,
-      Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,Format.Categorical,
-      Format.Categorical,Format.Categorical,Format.Categorical,Format.Continuous,Format.Continuous,Format.Continuous,Format.Continuous,
-      Format.Categorical)
+      for (line <- testSrc.getLines.drop(1))
+        testData += line.split(',').tail.map(_.trim.toFloat).toSeq
 
-    val input = args(2) match {
-      case "seq" => trainData.toArray.toSeq
-      case "par" => trainData.toArray.toSeq.par
+      // read mode changed because it got overhead error
+      //val x: Iterator[Seq[Float]] = trainSrc.getLines.drop(1).map(r => r.split(',').map(_.trim).tail.map(_.toFloat))
+      val featFormats = List(
+        Format.Continuous, Format.Continuous, Format.Categorical, Format.Continuous, Format.Continuous, Format.Continuous, Format.Continuous,
+        Format.Continuous, Format.Categorical, Format.Categorical, Format.Categorical, Format.Continuous, Format.Categorical,
+        Format.Continuous, Format.Categorical, Format.Continuous, Format.Categorical, Format.Continuous, Format.Continuous, Format.Categorical,
+        Format.Categorical, Format.Categorical, Format.Categorical, Format.Categorical, Format.Categorical, Format.Categorical,
+        Format.Continuous, Format.Continuous, Format.Categorical, Format.Categorical, Format.Categorical, Format.Categorical,
+        Format.Categorical, Format.Categorical, Format.Categorical, Format.Categorical, Format.Categorical, Format.Categorical,
+        Format.Categorical, Format.Categorical, Format.Categorical, Format.Categorical, Format.Categorical, Format.Categorical,
+        Format.Categorical, Format.Categorical, Format.Categorical, Format.Continuous, Format.Continuous, Format.Continuous, Format.Continuous,
+        Format.Categorical)
+
+      val input = args(2) match {
+        case "seq" => trainData.toArray.toSeq
+        case "par" => trainData.toArray.toSeq.par
+      }
+
+      println("Computation mode: " + args(2))
+
+      val c45: C45Alg = new base.C45
+
+      var t1 = System.nanoTime
+      val tree = c45.train(input, featFormats) //.show
+      val trainTime = System.nanoTime - t1
+
+      t1 = System.nanoTime
+      val predictedYs = tree.predict(testData)
+      val testTime = System.nanoTime - t1
+
+      val score = tree.score(testData, predictedYs)
+
+      println("{" +
+        "  trainTime: " + trainTime / 1e9d +
+        ", testTime: " + testTime / 1e9d +
+        ", score: " + score +
+        "}")
+
+      TreeSaver.save(tree, trainDSPath + ".tree")
+    } else {
+      val sc = ContextFactory.getContext(LogLevel.OFF)
+      val rdd = sc.textFile(trainDSPath)
+
+      val dataset = rdd
+        .mapPartitionsWithIndex{(idx, iter) => if(idx == 0) iter.drop(1) else iter}
+        .map(row => row.split(",").toSeq)
+        .map(_.drop(1))
+        .map(_.map(_.toFloat))
+
+      val c45 = new C45
+      c45.run(dataset)
+
+      System.in.read()
+
     }
-
-    println("Computation mode: " + args(2))
-
-    val c45 : C45Alg = new base.C45
-
-    var t1 = System.nanoTime
-    val tree = c45.train(input, featFormats) //.show
-    val trainTime = System.nanoTime - t1
-
-    t1 = System.nanoTime
-    val predictedYs = tree.predict(testData)
-    val testTime = System.nanoTime - t1
-
-    val score = tree.score(testData, predictedYs)
-
-    println("{" +
-      "  trainTime: "   + trainTime / 1e9d +
-      ", testTime: " + testTime / 1e9d +
-      ", score: " + score +
-      "}")
-
-    TreeSaver.save(tree, trainDSPath + ".tree")
 
 //    val sc = ContextFactory.getContext()
 //
