9abe07991b35b8cf39709dc6b3e31c86200c2fa8
Alessandro Giacche <25297222+alejack9@users.noreply.github.com>
2022-11-16 19:50:58 +0100

    fix pickle reader

--- external/pkl_to_csv.py
+++ external/pkl_to_csv.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 for size in [1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152]:
-    pd.from_pickle(f"./data/input_train_{size}.pkl").to_csv(f"./data/input_train_{size}.csv")
-    pd.from_pickle(f"./data/input_test_{size}.pkl").to_csv(f"./data/input_test_{size}.csv")
+    pd.read_pickle(f"./data/input_train_{size}.pkl").to_csv(f"./data/input_train_{size}.csv")
+    pd.read_pickle(f"./data/input_test_{size}.pkl").to_csv(f"./data/input_test_{size}.csv")
