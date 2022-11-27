bfb69e9fb239ef98cd0d9df5593416c346e9f457
Alessandro Giacchè <25297222+alejack9@users.noreply.github.com>
2022-11-27 19:20:23 +0100

    cloud runner template (todo tofinish)

--- scripts/cloudRunner.py
+++ scripts/cloudRunner.py
@@ -1,8 +1,25 @@
 import subprocess
 
-BUCKET_NAME='test'
+BUCKET_NAME='source'
+REGION='EUROPE-WEST8' # milan
+CLUSTER_NAME='Accidental-Severity-Prediction-Cluster'
+MASTER_MACHINE_TYPE='N2'
+WORKER_MACHINE_TYPE=MASTER_MACHINE_TYPE
+NUM_WORKERS='2'
+# TODO
+SP_MODE=''
+SR_MODE=''
+PARTITIONS=''
+
+INPUT_FILE_NAME="input_test_1024_binned.csv"
+JOB_ID=4242
+
+subprocess.call(['sbt', 'clean', 'assembly'])
+
+# Create the bucket for jar, input and output
 subprocess.call(['gsutil', 'mb', '-l', f'${REGION}', f'gs://${BUCKET_NAME}'])
 
+# Create the cluster
 subprocess.call(['gcloud',
                 'dataproc',
                 'clusters',
@@ -10,8 +27,6 @@ subprocess.call(['gcloud',
                 f'${CLUSTER_NAME}',
                 '--region',
                 f'${REGION}',
-                '--zone',
-                f'${ZONE}',
                 '--master-machine-type',
                 f'${MASTER_MACHINE_TYPE}',
                 '--num-workers',
@@ -20,9 +35,51 @@ subprocess.call(['gcloud',
                 f'${WORKER_MACHINE_TYPE}'
 ])
 
-subprocess.call(['sbt', 'clean', 'assembly'])
 subprocess.call(['gsutil', 
                 'cp', 
                 'target/scala-2.12/FinalProject-assembly-1.0.0.jar', 
                 f'gs://${BUCKET_NAME}/FinalProject-assembly-1.0.0.jar'
+])
+
+subprocess.call(['gsutil',
+                'cp',
+                f'data/${INPUT_FILE_NAME}input_test_1024_binned.csv',
+                f'gs://${BUCKET_NAME}/${INPUT_FILE_NAME}'
+])
+subprocess.call(['gcloud',
+                'dataproc',
+                'jobs',
+                'submit',
+                'spark',
+                f'--id ${JOB_ID}',
+                '--async',
+                f'--cluster=${CLUSTER_NAME}',
+                f'--region=${REGION}',
+                f'--jar=gs://${BUCKET_NAME}/FinalProject-assembly-1.0.0.jar',
+                '--',
+                'yarn',
+                f'gs://${BUCKET_NAME}/${INPUT_FILE_NAME}',
+                f'gs://${BUCKET_NAME}',
+                f'sp=${SP_MODE}',
+                f'sr=${SR_MODE}',
+                f'${PARTITIONS}'
+])
+
+subprocess.call(['gcloud',
+                'dataproc',
+                'clusters',
+                'delete',
+                f'${CLUSTER_NAME}',
+                '--region',
+                f'${REGION}',])
+
+subprocess.call(['gsutil',
+                'cp',
+                '-r',
+                f'gs://${BUCKET_NAME}/stayPoints data/.'
+])
+subprocess.call(['gsutil',
+                'cp',
+                '-r',
+                f'gs://${BUCKET_NAME}/stayRegions data/.'
 ])
