032eaac68b4f1713167ba91d2b054d0f35d7de46
Alessandro Giacche <25297222+alejack9@users.noreply.github.com>
2023-01-03 23:03:27 +0100

    readd continuous cases

--- src/main/scala/it/unibo/scalable/ml/dt/base/C45.scala
+++ src/main/scala/it/unibo/scalable/ml/dt/base/C45.scala
@@ -10,22 +10,22 @@ import scala.util.{Failure, Success, Try}
 
 class C45() extends C45Alg {
 
-//  private def bestContinuousSplitPoint[T <: Seq[Float]](ds: Dataset[T], dsEntropy: Float, attrValues: GenSeq[Float], attrIndex: Int)
-//  : (ContinuousCondition[Float], Float, Seq[Dataset[T]]) = {
-//    // https://stackoverflow.com/a/23847107 : best way to sort the array if par -> convert parseq to seq an then back to parseq
-//    // [1,2,3,4] -> [1,2,3] [2,3,4] -> [(1,2), (2,3), (3,4)] => [1.5, 2.5, 3.5]
-//
-//    val attrValuesSorted = attrValues.sort()
-//    val midPoints = attrValuesSorted.init.zip(attrValuesSorted.tail).map { case (a, b) => (a + b) / 2.0f }
-//
-//    val bestSplitPoint = midPoints.map(midpoint => {
-//      val partitions = ds.partition(row => row(attrIndex) < midpoint)
-//      val partList = List(partitions._1, partitions._2)
-//      (midpoint, Calc.infoGainRatio(dsEntropy, partList, ds.length), partList)
-//    }).maxBy(_._2)
-//
-//    (ContinuousCondition(attrIndex, bestSplitPoint._1), bestSplitPoint._2, bestSplitPoint._3)
-//  }
+  private def bestContinuousSplitPoint[T <: Seq[Float]](ds: Dataset[T], dsEntropy: Float, attrValues: GenSeq[Float], attrIndex: Int)
+  : (ContinuousCondition[Float], Float, Seq[Dataset[T]]) = {
+    // https://stackoverflow.com/a/23847107 : best way to sort the array if par -> convert parseq to seq an then back to parseq
+    // [1,2,3,4] -> [1,2,3] [2,3,4] -> [(1,2), (2,3), (3,4)] => [1.5, 2.5, 3.5]
+
+    val attrValuesSorted = attrValues.sort()
+    val midPoints = attrValuesSorted.init.zip(attrValuesSorted.tail).map { case (a, b) => (a + b) / 2.0f }
+
+    val bestSplitPoint = midPoints.map(midpoint => {
+      val partitions = ds.partition(row => row(attrIndex) < midpoint)
+      val partList = List(partitions._1, partitions._2)
+      (midpoint, Calc.infoGainRatio(dsEntropy, partList, ds.length), partList)
+    }).maxBy(_._2)
+
+    (ContinuousCondition(attrIndex, bestSplitPoint._1), bestSplitPoint._2, bestSplitPoint._3)
+  }
 
   // the last value of each sample represents the class target
   override def train[T <: Seq[Float]](ds: Dataset[T], attributeTypes: Seq[Format]): Tree[Float] = {
@@ -55,19 +55,19 @@ class C45() extends C45Alg {
             })
           ))
         }
-//        else {
-//          // just because bestContinuousSplitPoint is heavy
-//          if (attrValues.length == 1) return Success(LeafFactory.get(ds))
-//
-//          val (cond, _, subDss) = bestContinuousSplitPoint(ds, Calc.entropy(ds), attrValues, attrIndex)
-//
-//          return Success( CondNode(cond, subDss.map(_train(_, attributes, depth + 1) match {
-//            case Success(value) => value
-//            case Failure(_) =>
-//              // Stack overflow error, leaf created instead
-//              LeafFactory.get(ds)
-//          })) )
-//        }
+        else {
+          // just because bestContinuousSplitPoint is heavy
+          if (attrValues.length == 1) return Success(LeafFactory.get(ds))
+
+          val (cond, _, subDss) = bestContinuousSplitPoint(ds, Calc.entropy(ds), attrValues, attrIndex)
+
+          return Success( CondNode(cond, subDss.map(_train(_, attributes, depth + 1) match {
+            case Success(value) => value
+            case Failure(_) =>
+              // Stack overflow error, leaf created instead
+              LeafFactory.get(ds)
+          })) )
+        }
       }
 
       // + 1 attributes are available
@@ -85,15 +85,14 @@ class C45() extends C45Alg {
             // [feat value, [samples]]
             val branches = ds.groupBy(sample => sample(attrIndex)).toList.sortBy(_._1).map(_._2)
             (Calc.infoGainRatio(dsEntropy, branches, ds.length), cond, branches, index)
-          } else throw new Exception("ENABLE CONTINUOUS")
-//          else { // continuous case
-//            val (cond, infoGainRatio, subDss) = bestContinuousSplitPoint(ds, dsEntropy, attrValues, attrIndex)
-//            (infoGainRatio, cond, subDss, index)
-//          }
+          } else { // continuous case
+            val (cond, infoGainRatio, subDss) = bestContinuousSplitPoint(ds, dsEntropy, attrValues, attrIndex)
+            (infoGainRatio, cond, subDss, index)
+          }
         }
 
-      println("============== info gain ratios ")
-      println(infoGainRatios.map(t => (t._4, t._1)).sortBy(_._2).mkString(","))
+//      println("============== info gain ratios ")
+//      println(infoGainRatios.map(t => (t._4, t._1)).sortBy(_._2).mkString(","))
 
       if (infoGainRatios.forall(_._1 == 0.0f)) return Success(LeafFactory.get(ds))
 
