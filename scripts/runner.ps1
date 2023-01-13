c3012fe9cc816974b2002bf4c211580bd32e12d6
Alessandro Giacchè <25297222+alejack9@users.noreply.github.com>
2023-01-13 19:29:27 +0100

    new runner

--- scripts/runner.ps1
+++ scripts/runner.ps1
@@ -2,33 +2,34 @@
 $date = Get-Date -Format "yy-MM-dd HH:mm:ss"
 
 $dataPath = "./data"
-[String[]]$modes = "par", "seq"
+[String[]]$modes = "seq"
+# [String[]]$modes = "seq"
+
+Write-Host $PWD
 
 foreach ($x in $modes)
 {
-    [String[]]$nums = "65536", "131072", "262144", "524288", "1048576", "2097152"
+    [String[]]$nums = "32768"
+    # [String[]]$nums =  "16384", "8192", "4096", "2048", "1024"
     foreach ($num in $nums) {
-        $train_input = "$dataPath/input_train_$num.csv"
-        $test_input  = "$dataPath/input_test_$num.csv"
+        $train_input = "$dataPath/input_train_${num}.csv"
+        $test_input  = "$dataPath/input_test_${num}.csv"
         $pinfo = New-Object System.Diagnostics.ProcessStartInfo
         $pinfo.FileName = "C:\Program Files\Java\jre1.8.0_341\bin\java.exe"
         $pinfo.RedirectStandardError = $true
         $pinfo.RedirectStandardOutput = $true
         $pinfo.UseShellExecute = $false
-        $pinfo.Arguments = "-Xmx10G -Xss8G -jar ./target/scala-2.12/FinalProject-assembly-0.1.0-SNAPSHOT.jar $train_input $test_input $x"
+        $pinfo.Arguments = "-Xmx10G -Xss8G -jar .\target\scala-2.12\FinalProject-assembly-1.0.0.jar $train_input $test_input $x $dataPath\results\${num}_${x}_cont.log"
 
         $current = Get-Date -Format "HH:mm:ss"
-        "${date}_$current - Starting $x in $num..." | Tee-Object -Append "$dataPath/logs/${num}.log" | Write-Host
+        "${date}_$current - Starting $x in $num..." | Tee-Object -Append "$dataPath\logs\${num}_${x}_cont.log" | Write-Host
         $results = Invoke-Process $pinfo
-
-        "--------------StdOut-----------------" | Tee-Object -Append "$dataPath/logs/${num}.log" | Write-Host
-        $results.StdOut | Tee-Object -Append "$dataPath/logs/${num}.log" | Write-Host
-        "--------------StdErr-----------------" | Tee-Object -Append "$dataPath/logs/${num}.log" | Write-Host
-        $results.StdErr | Tee-Object -Append "$dataPath/logs/${num}.log" | Write-Host
-        "-------------ExitCode----------------" | Tee-Object -Append "$dataPath/logs/${num}.log" | Write-Host
-        $results.ExitCode | Tee-Object -Append "$dataPath/logs/${num}.log" | Write-Host
+        $results.StdOut | Tee-Object -Append "$dataPath\logs\${num}_${x}_cont.log" | Write-Host
+        $results.StdErr | Tee-Object -Append "$dataPath\logs\${num}_${x}_cont.log" | Write-Host
+        "-------------ExitCode----------------" | Tee-Object -Append "$dataPath\logs\${num}_${x}_cont.log" | Write-Host
+        $results.ExitCode | Tee-Object -Append "$dataPath\logs\${num}_${x}_cont.log" | Write-Host
         
         $current = Get-Date -Format "HH:mm:ss"
-        "${date}_$current - ... $x in $num - Done" | Tee-Object -Append "$dataPath/logs/${num}.log" | Write-Host
+        "${date}_$current - ... $x in $num - Done" | Tee-Object -Append "$dataPath\logs\${num}_${x}_cont.log" | Write-Host
     }    
 }
