02194b21c2abb75326954e2e667f1eb6d5426aaf
Alessandra Boccuto <aless97b@gmail.com>
2023-01-14 00:55:59 +0100

    script refactoring

--- scripts/data_preparation/kde_analysis.py
+++ scripts/data_preparation/kde_analysis.py
@@ -15,16 +15,16 @@ continuousIndexes = [
 
 continuousIndexes = [i for i, x in enumerate(continuousIndexes) if x == "Continuous"]
 
-print(continuousIndexes)
-
 df1 = pd.read_csv("./data/input_train_1024.csv").drop("Accident_Index", axis=1)
 df2 = pd.read_csv("./data/input_train_2048.csv").drop("Accident_Index", axis=1)
 
 for index in continuousIndexes:
     plt.figure(figsize=(8, 4))
     f, axes = plt.subplots(2, 1)
+
     sns.kdeplot(df1.iloc[:, index], ax=axes[0]).set(title=f"df1 - {df1.columns[index]}")
     sns.kdeplot(df2.iloc[:, index], ax=axes[1]).set(title=f"df2 - {df2.columns[index]}")
+
     plt.savefig(f"./plots/{df1.columns[index]}.png")
     plt.close()
 
--- scripts/data_preparation/pkl_to_csv.py
+++ scripts/data_preparation/pkl_to_csv.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 for size in [1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152]:
     pd.read_pickle(f"./data/input_train_{size}.pkl").to_csv(f"./data/input_train_{size}.csv")
     pd.read_pickle(f"./data/input_test_{size}.pkl").to_csv(f"./data/input_test_{size}.csv")
