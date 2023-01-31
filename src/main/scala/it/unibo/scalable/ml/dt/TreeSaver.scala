42cf2fed49dd6dab34d01e79d5ddbfbcb980349d
Alessandro Giacchè <25297222+alejack9@users.noreply.github.com>
2023-01-31 21:27:52 +0100

    fix treesaver gen type

--- src/main/scala/it/unibo/scalable/ml/dt/TreeSaver.scala
+++ src/main/scala/it/unibo/scalable/ml/dt/TreeSaver.scala
@@ -7,7 +7,7 @@ import scala.collection.GenSeq
 object TreeSaver {
 
   @tailrec
-  private def saveTree[T](nodes: GenSeq[(Int, Tree[T])], pw: PrintWriter): Unit = nodes match {
+  private def saveTree[T <: AnyVal](nodes: GenSeq[(Int, Tree[T])], pw: PrintWriter): Unit = nodes match {
     case x :: xs => x match {
       case (depth, CondNode(cond, children)) =>
         pw.write(f"$depth - ${cond.toString} | ")
@@ -19,12 +19,12 @@ object TreeSaver {
     case Nil =>
   }
 
-  def save[T](tree: Tree[T], pw: PrintWriter): Unit = {
+  def save[T <: AnyVal](tree: Tree[T], pw: PrintWriter): Unit = {
     pw.write(tree.toYaml())
     pw.close()
   }
 
-  def save[T](tree: Tree[T], dest: String): Unit = {
+  def save[T <: AnyVal](tree: Tree[T], dest: String): Unit = {
     val pw = new PrintWriter(new File(dest))
     save(tree, pw)
   }
